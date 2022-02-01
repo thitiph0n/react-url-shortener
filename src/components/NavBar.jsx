f57260f40090386b5ab4f054b7504ad264176864
thitiph0n <thitiphon.ks@gmail.com>
2022-02-01 17:05:16 +0700

    feat: add link to logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div className='w-full bg-white h-16 shadow-md flex items-center justify-center px-3 fixed'>
-      <div className='flex flex-row items-center cursor-pointer'>
+      <Link to='/' className='flex flex-row items-center cursor-pointer'>
         <Icon icon='emojione-v1:speak-no-evil-monkey' width='40' height='40' />
         <h1 className='ml-1 text-2xl font-bold uppercase text-yellow-800'>
           Links
         </h1>
-      </div>
+      </Link>
       {/* <button className='bg-orange-400 text-white font-semibold hover:bg-orange-300 px-4 py-2 rounded-lg '>
         Login
       </button> */}
